6df8c25d65e0733c79d5c22ae29e0cb62954c2a2
Aditya <84168117+AdityaTheDev@users.noreply.github.com>
2025-01-19 07:16:22 +0530

    Add files via upload

---
+++ Data Structures & Algorithms in Python/heap_tree_in_python(min_heap).py
@@ -0,0 +1,57 @@
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, value):
+        self.heap.append(value)
+        self._heapify_up(len(self.heap) - 1)
+
+    def delete_min(self):
+        if not self.heap:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()  # Replace root with last element
+        self._heapify_down(0)
+        return root
+
+    def _heapify_up(self, index):
+        parent = (index - 1) // 2
+        while index > 0 and self.heap[index] < self.heap[parent]:
+            self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
+            index = parent
+            parent = (index - 1) // 2
+
+    def _heapify_down(self, index):
+        smallest = index
+        left = 2 * index + 1
+        right = 2 * index + 2
+
+        if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
+            smallest = left
+        if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+
+        if smallest != index:
+            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+            self._heapify_down(smallest)
+
+    def get_min(self):
+        return self.heap[0] if self.heap else None
+
+    def display(self):
+        print("Heap:", self.heap)
+
+
+if __name__ == "__main__":
+    heap = MinHeap()
+    for num in [10, 5, 20, 2, 15]:
+        heap.insert(num)
+        heap.display()
+
+    print("Minimum element:", heap.get_min())
+    while heap.heap:
+        print("Removed:", heap.delete_min())
+        heap.display()
